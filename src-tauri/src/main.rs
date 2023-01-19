c9e36cc224a4fd6dfb458572fcd8994f84e8b9c0
tuip123-xzhh <tuip123@outlook.com>
2023-01-19 15:23:53 +0800

    feat:初始化db

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,25 +2,65 @@
 all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
-
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn test(){
-    let conn = sqlite::open("test.db").unwrap();
-    let query = "
-    CREATE TABLE users (name TEXT, age INTEGER);
-    INSERT INTO users VALUES ('Alice', 42);
-    INSERT INTO users VALUES ('Bob', 69);
-";
-    conn.execute(query).unwrap();
+use sqlite::{State};
+use std::fs::create_dir_all;
+use std::path::Path;
+use tauri::api::path::app_data_dir;
+
+fn init_db(){
+    let dir = app_data_dir(&Default::default()).unwrap();
+    let full_dir = dir.to_str().unwrap().to_owned()+"tuip123-comic\\";
+    let p = Path::new(&full_dir);
+    match create_dir_all(p){
+        Ok(_f)=>{},
+        Err(err)=>{
+            println!("{:?}",err)
+        }
+    }
+
+    let conn = sqlite::open(full_dir.to_owned()+"test.db").unwrap();
+
+    let mut query = "SELECT name FROM sqlite_master WHERE type='table' AND name='comic' COLLATE NOCASE";
+    let mut statement = conn
+        .prepare(query)
+        .unwrap();
+    let mut b = false;
+    while let State::Row = statement.next().unwrap() {
+        b = true;
+    }
+    if !b {
+        query = "CREATE TABLE comic (Id INTEGER,path TEXT,cover TEXT,count INTEGER,libraryId INTEGER)";
+        conn.execute(query).unwrap();
+    }
+
+    query = "SELECT name FROM sqlite_master WHERE type='table' AND name='library' COLLATE NOCASE";
+    statement = conn
+        .prepare(query)
+        .unwrap();
+    b = false;
+    while let State::Row = statement.next().unwrap() {
+        b = true;
+    }
+    if !b {
+        query = "CREATE TABLE library (Id INTEGER,root TEXT)";
+        conn.execute(query).unwrap();
+    }
 }
+
+#[tauri::command]
+fn add_library(path:&str){
+    println!("{}", path)
+}
+
 fn main() {
+    init_db();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,test])
+        .invoke_handler(tauri::generate_handler![greet,add_library])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,25 +1,33 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
+import {ref} from "vue";
+import {invoke} from "@tauri-apps/api/tauri";
+import {open} from '@tauri-apps/api/dialog';
 
 const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  greetMsg.value = await invoke("greet", {name: name.value});
 }
 
-async function createTable() {
-  await invoke('test')
+async function addLibrary() {
+  const selected = await open({
+    multiple: true,
+    directory: true
+  }) as string[];
+  for (let string of selected) {
+    await invoke("add_library", {path: string})
+  }
 }
+
 </script>
 
 <template>
   <div class="card">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <input id="greet-input" v-model="name" placeholder="Enter a name..."/>
     <button type="button" @click="greet()">Greet</button>
-    <button type="button" @click="createTable()">createTable</button>
+    <button type="button" @click="addLibrary()">addLibrary</button>
   </div>
 
   <p>{{ greetMsg }}</p>
