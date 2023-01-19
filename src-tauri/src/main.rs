62e68b3cacb9deb7ed4e1da1e3a36277fc9bb84f
tuip123-xzhh <tuip123@outlook.com>
2023-01-19 12:30:14 +0800

    feat:添加sqlite

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,26 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn test(){
+    let conn = sqlite::open("test.db").unwrap();
+    let query = "
+    CREATE TABLE users (name TEXT, age INTEGER);
+    INSERT INTO users VALUES ('Alice', 42);
+    INSERT INTO users VALUES ('Bob', 69);
+";
+    conn.execute(query).unwrap();
+}
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -9,12 +9,17 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsg.value = await invoke("greet", { name: name.value });
 }
+
+async function createTable() {
+  await invoke('test')
+}
 </script>
 
 <template>
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="button" @click="greet()">Greet</button>
+    <button type="button" @click="createTable()">createTable</button>
   </div>
 
   <p>{{ greetMsg }}</p>
